9eac437b9053505bcad75e9c0363c6f5128736f3
Sayan Saha <sayan.jubiee@gmail.com>
2024-07-30 09:33:08 -0400

    Update build_windows_ci.sh

--- build_tools/python_deploy/build_windows_ci.sh
+++ build_tools/python_deploy/build_windows_ci.sh
@@ -3,7 +3,7 @@ set -eo pipefail
 
 echo "Building torch-mlir"
 
-cmake -GNinja -Bbuild \
+cmake -GNinja -Bbuild_win \
   -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_C_COMPILER_LAUNCHER=ccache \
   -DCMAKE_CXX_COMPILER_LAUNCHER=ccache \
@@ -14,8 +14,9 @@ cmake -GNinja -Bbuild \
   -DLLVM_EXTERNAL_PROJECTS="torch-mlir" \
   -DLLVM_EXTERNAL_TORCH_MLIR_SOURCE_DIR="$PWD" \
   -DPython3_EXECUTABLE="$(which python)" \
+  -DLLVM_ENABLE_ASSERTIONS=ON \
   $GITHUB_WORKSPACE/externals/llvm-project/llvm
 
-cmake --build build --config Release
+cmake --build build_win --config Release
 
 echo "Build completed successfully"
